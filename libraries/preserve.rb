5dc5d4bab070efaa8c9b19a9dbc1368664cf36b9
Soban Vuex <soban.vuex@gmail.com>
2012-11-28 02:55:48 -0500

    Helper function

---
+++ libraries/preserve.rb
@@ -0,0 +1,17 @@
+def preserve (source_file = nil, destination_file = nil)
+  if !node[:nginx][:preserve_files] || destination_file.nil?
+    return false
+  end
+
+  exists  = File.exists? destination_file
+
+  if exists && node[:nginx][:preserve_files] && !source_file
+    return true
+  end
+
+  if exists && `diff #{destination_file} #{destination_file} -qN`.empty?
+    return false
+  end
+
+  return true
+end

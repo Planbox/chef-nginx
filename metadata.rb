a9fddb3b0dbdbc18c2191e666e2dfe38b3435227
Soban Vuex <soban.vuex@gmail.com>
2012-11-28 04:10:50 -0500

    .

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,3 @@ license           "MIT"
 description       "Install Nginx from source"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
-
-
-provides          "nginx"
-provides          "nginx::dependencies"
-provides          "nginx::source"
-provides          "nginx::modules"
-provides          "nginx::install"

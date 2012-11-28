b35fa0ed386de4a3347393a423af6a8e1f0a27c0
Soban Vuex <soban.vuex@gmail.com>
2012-11-28 03:51:16 -0500

    FC007: Ensure recipe dependencies are reflected in cookbook metadata

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,9 @@ license           "MIT"
 description       "Install Nginx from source"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
+
+
+provides          "nginx::dependencies"
+provides          "nginx::source"
+provides          "nginx::modules"
+provides          "nginx::install"

4947482bf45fc86954a5a4564f0d3e22cad0d791
Soban Vuex <soban.vuex@gmail.com>
2012-11-28 04:02:13 -0500

    FC007: Ensure recipe dependencies are reflected in cookbook metadata

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name              "nginx"
 maintainer        "Soban Vuex"
 maintainer_email  "soban.vuex@gmail.com"
 license           "MIT"
@@ -6,6 +7,7 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 
 
+provides          "nginx"
 provides          "nginx::dependencies"
 provides          "nginx::source"
 provides          "nginx::modules"

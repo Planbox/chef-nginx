4913c912884f69a5c420f7404db30d61262876b6
Soban Vuex <soban.vuex@gmail.com>
2012-12-04 22:55:45 -0500

    parentheses typo

--- recipes/modules.rb
+++ recipes/modules.rb
@@ -1,9 +1,9 @@
 # Modules
 #
-node['nginx']['user_modules'].each do |(modname, (modgit, modver)), moddeps|
+node['nginx']['user_modules'].each do |(modname, (modgit, modver), moddeps)|
   git "#{Chef::Config[:file_cache_path]}/nginx-#{modname}" do
     repository  modgit
-    reference   modver
+    revision    modver
     action      :sync
   end
 
